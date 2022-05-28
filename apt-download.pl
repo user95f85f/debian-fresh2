4add228c8164ddcb478e3ad8ae9b9db584c1b295
user95f85f <48297795+user95f85f@users.noreply.github.com>
2022-05-28 21:20:04 +0000

    Add files via upload

---
+++ apt-download.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+
+use strict;
+use warnings;
+use Data::Dumper;
+$Data::Dumper::Maxdepth = 1;
+use AptPkg::Cache;
+use autodie;
+use List::Util qw/uniq/;
+
+
+#<config>
+my $package_name = $ARGV[0] || 'kate';
+my $target_arch = 'amd64';
+#</config>
+
+my $cache = AptPkg::Cache->new();
+
+sub get_pkgname_depends_list{
+  my ($_pkgname) = @_;
+  if(!defined($_pkgname)){
+    warn "WARNING: package name is undefined........\n";
+    return ();
+  }
+  if(length($_pkgname) == 0){
+    warn "WARNING: package name is empty...........\n";
+    return ();
+  }
+  my $version_list = $cache->get($_pkgname)->{'VersionList'};
+  if(!defined($version_list)){
+    warn "WARNING: Version list for package name $_pkgname is undefined.\n";
+    return ();
+  }
+  if(scalar(@{$version_list}) == 0){
+    warn "WARNING: Version list for package name $_pkgname is empty.\n";
+    return ();
+  }
+  my $dependencies = $version_list->[0]->{'DependsList'};
+  if(!defined($dependencies)){
+    warn "WARNING: Dependencies list for package name $_pkgname is undefined.\n";
+    return ();
+  }
+  if(scalar(@{$dependencies}) == 0){
+    warn "WARNING: Dependencies list for package name $_pkgname is empty.\n";
+    return ();
+  }
+  return @{$dependencies};
+}
+
+
+my @dependencies = get_pkgname_depends_list($package_name);
+my @package_names = ($package_name);
+while(1){
+  $_ = shift(@dependencies) or last;
+  if($_->{'DepType'} ne 'Depends'){next;}
+  my $target_pkg = $_->{'TargetPkg'};
+  if($target_pkg->{'Arch'} ne $target_arch){next;}
+  if($target_pkg->{'CurrentState'} eq 'Installed'){next;}
+  push(@package_names, $target_pkg->{'Name'});
+  push(@dependencies, get_pkgname_depends_list($target_pkg->{'Name'}));
+}
+
+print 'sudo apt download ', join(' ', uniq(sort(@package_names))), "\n";
